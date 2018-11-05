959757442df7ed65301f3f87eac4b5cb9a9daed6
danamart <danamart14@gmail.com>
2018-11-04 18:35:05 -0700

    still not working

--- climate_app.py
+++ climate_app.py
@@ -15,7 +15,7 @@ app = Flask(__name__)
 #Return the JSON representation of your dictionary.
 def prcp():
     previous_year = dt.date.today() - dt.timedelta(days=365)
-    prcp_year = session.query(Measurement.date,func.sum(Measurement.prcp)).filter(Measurement.date >= previous_year).group_by(Measurement.date).order_by(Measurement.date).all()
+    prcp_year = session.query(measurement.date,func.sum(measurement.prcp)).filter(measurement.date >= previous_year).group_by(measurement.date).order_by(measurement.date).all()
 
     prcp_dict = dict(prcp_year)
     return jsonify(prcp_dict)
@@ -30,13 +30,13 @@ def stations():
     return jsonify (station_list)
 
 
-@app.route("/api/v1.0/tobs")
+#@app.route("/api/v1.0/tobs")
 
 #query for the dates and temperature observations from a year from the last data point.
 #Return a JSON list of Temperature Observations (tobs) for the previous year.
 
-@app.route("/api/v1.0/<start>")
-@app.route("/api/v1.0/<start>/<end>")
+#@app.route("/api/v1.0/<start>")
+#@app.route("/api/v1.0/<start>/<end>")
 
 #Return a JSON list of the minimum temperature, the average temperature, and the max 
 #temperature for a given start or start-end range.
@@ -44,6 +44,7 @@ def stations():
 #and equal to the start date.
 #When given the start and the end date, calculate the TMIN, TAVG, and TMAX for dates 
 #between the start and end date inclusive.
+
 
 if __name__ == "__main__":
     app.run(debug=True)
